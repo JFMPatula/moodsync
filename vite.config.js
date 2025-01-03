e308e256ca3b5c5dcb629b2087929618698d850a
JFMPatula <jeanfaithmarie.patula@carsu.edu.ph>
2025-01-04 00:17:31 +0800

    Fixing deployment9

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/' : '/', // Adjust base URL if needed
-    build: {
-        outDir: 'public/build',  // Output to the public/build directory
-    },
+    
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         vue({
@@ -22,6 +19,11 @@ export default defineConfig({
         }),
     ],
     server: {
-        hmr: false, // Disable HMR in production
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true,
+        },
     },
 });
